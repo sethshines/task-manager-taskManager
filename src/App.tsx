f03219d7363ceea70a7458645bb38b902fb66c89
Rahul Seth <sethshines@gmail.com>
2023-03-08 19:47:38 +0530

    Initial Commit task

---
+++ src/App.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import TaskManager from "./Components/TaskManager/taskmanager";
+
+const App = () => <TaskManager />;
+ReactDOM.render(<App />, document.getElementById("app"));
---
+++ src/Components/TaskManager/taskmanager.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.css';
+import "../../index.css";
+
+export default function TaskManager() {
+  return (
+    <div className="container-fluide" style={{backgroundColor: '#3e3e3e'}}>
+        <h1 className="text-center" style={{color: '#fff'}}> Task Manager</h1>
+    </div>
+  )
+}
